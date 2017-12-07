df89457bf1d5cb9e4018773f834a59624037aeb6
yunus almaliq <33022312+yunusalmaliq@users.noreply.github.com>
2017-12-07 17:19:13 +0700

    Add files via upload

---
+++ desimal.py
@@ -0,0 +1,9 @@
+print ("Angka Binary diawali 0b")
+print ("Pilih [1]Desimal ke Binary")
+print ("Pilih [2]exit")
+i = int(input("Masukkan pilihan anda: "))
+if i == 1:
+    x = int(input("masukkan angka Desimal: "))
+    print (bin(x))
+else:
+    print("bye bye")
